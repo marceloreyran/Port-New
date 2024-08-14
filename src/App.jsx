30fe535d2a8a7e19cab8ccc7e8dd4cbbe110cdd2
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2024-08-14 10:12:14 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+
+  
+  return (
+  <div className={styles.App} >
+  <Navbar>
+    
+  </Navbar>
+
+  </div>
+  )
+}
+
+export default App
